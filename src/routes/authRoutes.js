3ce7c6df6c91527962eff82595201015915c9c3c
sbalazs222 <sbalazs222@gmail.com>
2026-03-06 11:25:37 +0100

    finalized auth endpoints

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 
 import { login, register, logout } from '../controllers/authController.js';
+import { validateFieldCount, validateRequiredFields}  from "psgutil";
 
 const router = express.Router();
 
-router.post('/register', register);
-router.post('/login', login);
+router.post('/register', validateFieldCount(3), validateRequiredFields(['username', 'password']), register);
+router.post('/login', validateFieldCount(2), validateRequiredFields(['username', 'password']), login);
 router.post('/logout', logout);
 
 export default router;
